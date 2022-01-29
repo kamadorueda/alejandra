5ffcf045793d9014f23ea1b9bf20b3ef7796baa4
Kevin Amado <kamadorueda@gmail.com>
2022-01-29 18:16:22 -0500

    feat: increase paren coverage

--- src/rules/paren.rs
+++ src/rules/paren.rs
@@ -12,41 +12,45 @@ pub fn rule(
         build_ctx.config.layout()
     };
 
+    // (
     let child = children.get_next().unwrap();
     steps.push_back(crate::builder::Step::Format(child.element));
     match layout {
         crate::config::Layout::Tall => {
             steps.push_back(crate::builder::Step::Indent);
-            steps.push_back(crate::builder::Step::NewLine);
-            steps.push_back(crate::builder::Step::Pad);
         }
-        crate::config::Layout::Wide => {
-            steps.push_back(crate::builder::Step::Whitespace);
-        }
+        crate::config::Layout::Wide => {}
     }
 
+    // /**/
     children.drain_comments(|text| {
-        steps.push_back(crate::builder::Step::Comment(text));
         steps.push_back(crate::builder::Step::NewLine);
         steps.push_back(crate::builder::Step::Pad);
+        steps.push_back(crate::builder::Step::Comment(text));
     });
 
+    // expr
     let child = children.get_next().unwrap();
     match layout {
         crate::config::Layout::Tall => {
+            steps.push_back(crate::builder::Step::NewLine);
+            steps.push_back(crate::builder::Step::Pad);
             steps.push_back(crate::builder::Step::FormatWider(child.element));
         }
         crate::config::Layout::Wide => {
+            steps.push_back(crate::builder::Step::Whitespace);
             steps.push_back(crate::builder::Step::Format(child.element));
         }
     }
 
+    // /**/
     children.drain_comments(|text| {
         steps.push_back(crate::builder::Step::NewLine);
         steps.push_back(crate::builder::Step::Pad);
         steps.push_back(crate::builder::Step::Comment(text));
     });
 
+    // )
     let child = children.get_next().unwrap();
     match layout {
         crate::config::Layout::Tall => {
