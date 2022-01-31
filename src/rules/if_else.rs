dee5de12d69b74d66e69472a4e5b91ada9357f29
Kevin Amado <kamadorueda@gmail.com>
2022-01-30 22:38:14 -0500

    feat: expand all if/then/else

--- src/rules/if_else.rs
+++ src/rules/if_else.rs
@@ -6,24 +6,11 @@ pub fn rule(
 
     let mut children = crate::children::Children::new(build_ctx, node);
 
-    let layout = if children.has_comments() {
-        &crate::config::Layout::Tall
-    } else {
-        build_ctx.config.layout()
-    };
-
     for branch in ["if", "then", "else"] {
         // if/then/else
         let child = children.get_next().unwrap();
         steps.push_back(crate::builder::Step::Format(child.element));
-        match layout {
-            crate::config::Layout::Tall => {
-                steps.push_back(crate::builder::Step::Indent);
-            }
-            crate::config::Layout::Wide => {
-                steps.push_back(crate::builder::Step::Whitespace);
-            }
-        }
+        steps.push_back(crate::builder::Step::Indent);
 
         // /**/
         children.drain_comments(|text| {
@@ -34,28 +21,15 @@ pub fn rule(
 
         // expr
         let child = children.get_next().unwrap();
-        match layout {
-            crate::config::Layout::Tall => {
-                steps.push_back(crate::builder::Step::NewLine);
-                steps.push_back(crate::builder::Step::Pad);
-                steps.push_back(crate::builder::Step::FormatWider(
-                    child.element,
-                ));
-                steps.push_back(crate::builder::Step::Dedent);
-                if branch != "else" {
-                    steps.push_back(crate::builder::Step::NewLine);
-                    steps.push_back(crate::builder::Step::Pad);
-                }
-            }
-            crate::config::Layout::Wide => {
-                steps.push_back(crate::builder::Step::Format(child.element));
-                if branch != "else" {
-                    steps.push_back(crate::builder::Step::Whitespace);
-                }
-            }
-        }
+        steps.push_back(crate::builder::Step::NewLine);
+        steps.push_back(crate::builder::Step::Pad);
+        steps.push_back(crate::builder::Step::FormatWider(child.element));
+        steps.push_back(crate::builder::Step::Dedent);
 
         if branch != "else" {
+            steps.push_back(crate::builder::Step::NewLine);
+            steps.push_back(crate::builder::Step::Pad);
+
             // /**/
             children.drain_comments(|text| {
                 steps.push_back(crate::builder::Step::Comment(text));
