47936c83fe20182ce65c5ac9c3fd4815b936c0fc
Kevin Amado <kamadorueda@gmail.com>
2022-02-09 22:05:23 -0500

    feat: keep funcs implementations on same line

--- src/rules/lambda.rs
+++ src/rules/lambda.rs
@@ -47,14 +47,7 @@ pub fn rule(
 
     // :
     let child = children.get_next().unwrap();
-    match layout {
-        crate::config::Layout::Tall => {
-            steps.push_back(crate::builder::Step::FormatWider(child.element));
-        }
-        crate::config::Layout::Wide => {
-            steps.push_back(crate::builder::Step::Format(child.element));
-        }
-    }
+    steps.push_back(crate::builder::Step::Format(child.element));
 
     // /**/
     children.drain_comments_and_newlines(|element| match element {
@@ -71,19 +64,56 @@ pub fn rule(
     let child = children.get_next().unwrap();
     match layout {
         crate::config::Layout::Tall => {
-            if is_pattern_type {
-                steps.push_back(crate::builder::Step::NewLine);
-                steps.push_back(crate::builder::Step::Pad);
-            } else if let rnix::SyntaxKind::TOKEN_COMMENT
-            | rnix::SyntaxKind::TOKEN_WHITESPACE =
-                child_prev.element.kind()
+            if is_pattern_type
+                || matches!(
+                    child_prev.element.kind(),
+                    rnix::SyntaxKind::TOKEN_COMMENT
+                        | rnix::SyntaxKind::TOKEN_WHITESPACE
+                )
+                || (matches!(
+                    child.element.kind(),
+                    rnix::SyntaxKind::NODE_LAMBDA
+                ) && matches!(
+                    child
+                        .element
+                        .clone()
+                        .into_node()
+                        .unwrap()
+                        .children()
+                        .next()
+                        .unwrap()
+                        .kind(),
+                    rnix::SyntaxKind::NODE_PATTERN
+                ))
+                || !matches!(
+                    child.element.kind(),
+                    rnix::SyntaxKind::NODE_ATTR_SET
+                        | rnix::SyntaxKind::NODE_PAREN
+                        | rnix::SyntaxKind::NODE_LAMBDA
+                        | rnix::SyntaxKind::NODE_LET_IN
+                        | rnix::SyntaxKind::NODE_LIST
+                        | rnix::SyntaxKind::NODE_LITERAL
+                        | rnix::SyntaxKind::NODE_STRING
+                )
             {
+                if build_ctx.pos_new.column > 1 {
+                    steps.push_back(crate::builder::Step::Indent);
+                }
+
                 steps.push_back(crate::builder::Step::NewLine);
                 steps.push_back(crate::builder::Step::Pad);
+                steps.push_back(crate::builder::Step::FormatWider(
+                    child.element,
+                ));
+                if build_ctx.pos_new.column > 1 {
+                    steps.push_back(crate::builder::Step::Dedent);
+                }
             } else {
                 steps.push_back(crate::builder::Step::Whitespace);
+                steps.push_back(crate::builder::Step::FormatWider(
+                    child.element,
+                ));
             }
-            steps.push_back(crate::builder::Step::FormatWider(child.element));
         }
         crate::config::Layout::Wide => {
             steps.push_back(crate::builder::Step::Whitespace);
