d7e9fddd40ecbb4c481f16d378731d970a121677
Kevin Amado <kamadorueda@gmail.com>
2022-02-19 11:16:54 -0500

    feat: shaped with love by more people

--- src/cli.rs
+++ src/cli.rs
@@ -16,11 +16,18 @@ pub fn parse(args: Vec<String>) -> clap::ArgMatches {
         )
         .term_width(80)
         .after_help(indoc::indoc!(
+            // Let's just use the same sorting as on GitHub
+            //
+            // There are some non-code contributors,
+            // I'm sorting those subjectively
+            //
+            // Feel free to add here your contact/blog/links if you want
             "
             Shaped with love by:
             - Kevin Amado ~ @kamadorueda on GitHub, matrix.org and Gmail.
             - Thomas Bereknyei ~ @tomberek on GitHub and matrix.org.
             - David Arnold ~ @blaggacao on GitHub and matrix.org.
+            - Vincent Ambo ~ @tazjin on GitHub.
             - Mr Hedgehog ~ @ModdedGamers on GitHub.
             "
         ))
