c54e6c0a714ada10712ccba70018a5a9285dad1e
Kevin Amado <kamadorueda@gmail.com>
2022-02-20 19:03:34 -0500

    feat: release 0.3.1

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
   outputs = inputs: let
     commit = inputs.self.shortRev or "dirty";
     date = inputs.self.lastModifiedDate or inputs.self.lastModified or "19700101";
-    version = "0.3.0+${builtins.substring 0 8 date}.${commit}";
+    version = "0.3.1+${builtins.substring 0 8 date}.${commit}";
 
     nixpkgsForHost = host:
       import inputs.nixpkgs {
