e661731aac0f46e48c69562b4d040b60b9daa74f
Kevin Amado <kamadorueda@gmail.com>
2022-02-20 19:42:42 -0500

    feat: add a text user interface

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,3 @@
-use rayon::prelude::*;
-use std::io::Read;
-
 fn main() -> std::io::Result<()> {
     let matches = alejandra::cli::parse(std::env::args().collect());
 
@@ -12,36 +9,17 @@ fn main() -> std::io::Result<()> {
             let paths: Vec<String> =
                 alejandra::find::nix_files(paths.collect());
 
-            eprintln!("Formatting {} files.", paths.len());
-
-            let (results, errors): (Vec<_>, Vec<_>) = paths
-                .par_iter()
-                .map(|path| -> std::io::Result<bool> {
-                    alejandra::format::file(&config, path.to_string()).map(
-                        |changed| {
-                            if changed {
-                                eprintln!("Formatted: {}", &path);
-                            }
-                            changed
-                        },
-                    )
-                })
-                .partition(Result::is_ok);
-            eprintln!(
-                "Errors/Changed/Formatted: {}/{}/{}",
-                errors.len(),
-                results.into_iter().map(Result::unwrap).filter(|&x| x).count(),
-                paths.len()
-            );
+            if atty::is(atty::Stream::Stderr)
+                && atty::is(atty::Stream::Stdin)
+                && atty::is(atty::Stream::Stdout)
+            {
+                alejandra::cli::tui(config, paths)?;
+            } else {
+                alejandra::cli::simple(config, paths)?;
+            }
         }
         None => {
-            eprintln!("Formatting stdin.");
-            let mut stdin = String::new();
-            std::io::stdin().read_to_string(&mut stdin).unwrap();
-            print!(
-                "{}",
-                alejandra::format::string(&config, "stdin".to_string(), stdin)
-            );
+            alejandra::cli::stdin(config)?;
         }
     }
 
