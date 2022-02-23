9782e5ad4b5e9e17dbe39f7b9026088c5f38ebcf
Kevin Amado <kamadorueda@gmail.com>
2022-02-23 16:40:37 -0500

    feat: release 0.5.0

--- installers/aarch64-linux.sh
+++ installers/aarch64-linux.sh
@@ -2,7 +2,7 @@
 
 releases=https://github.com/kamadorueda/alejandra/releases/download/
 target=alejandra-aarch64-unknown-linux-musl
-version=0.4.0
+version=0.5.0
 
 curl -o alejandra -L "${releases}/${version}/${target}"
 
