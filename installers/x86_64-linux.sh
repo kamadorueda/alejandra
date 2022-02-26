a8bd2c82e4dec5631d7985e280185216b5080dd4
Kevin Amado <kamadorueda@gmail.com>
2022-02-26 14:08:40 -0500

    docs: simplify installation
    - I was running Alejandra on my android mobile phone,
      and the installers were not helpful, so lets
      just stick to the basics

--- installers/x86_64-linux.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-
-releases=https://github.com/kamadorueda/alejandra/releases/download/
-target=alejandra-x86_64-unknown-linux-musl
-version=0.6.0
-
-curl -o alejandra -L "${releases}/${version}/${target}"
-
-chmod +x alejandra
