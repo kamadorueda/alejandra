0b480bc36d720547314c66fa309283074a85fbb6
Kevin Amado <kamadorueda@gmail.com>
2022-02-28 17:12:13 -0500

    feat: simplify parens

---
+++ src/alejandra_engine/src/rules/inherit_from.rs
@@ -0,0 +1,6 @@
+pub(crate) fn rule(
+    build_ctx: &crate::builder::BuildCtx,
+    node: &rnix::SyntaxNode,
+) -> std::collections::LinkedList<crate::builder::Step> {
+    crate::rules::paren::rule_with_configuration(build_ctx, node, false)
+}
