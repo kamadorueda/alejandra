131c177c90244e3e270440ba8ad7f51a717686a7
Kevin Amado <kamadorueda@gmail.com>
2022-05-06 16:06:11 -0600

    feat: more robust pre-commit hook

--- .pre-commit-entry.sh
+++ .pre-commit-entry.sh
@@ -1,3 +1,26 @@
 #! /usr/bin/env sh
 
-nix run github:kamadorueda/alejandra -- -q "$@"
+set -eux
+
+if ! command -v nix-build; then
+  echo 'ERROR: this pre-commit hook requires "nix-build" to be installed first'
+  exit 1
+fi
+
+if !command -v nix-instantiate; then
+  echo 'ERROR: this pre-commit hook requires "nix-instantiate" to be installed first'
+  exit 1
+fi
+
+echo INFO: computing current system
+system="$(nix-instantiate --eval --expr builtins.currentSystem)"
+
+echo INFO: building Alejandra
+
+nix-build \
+  --attr ${system} \
+  --out-link result-alejandra \
+  https://github.com/kamadorueda/alejandra/tarball/1.2.0
+
+echo INFO: running Alejandra:
+result-alejandra/bin/alejandra -- -q "${@}"
